d32bfd308de287dea35afe4071da8efc52a113f1
Bram Borggreve <borggreve@gmail.com>
2019-12-28 14:56:12 -0500

    fix routes to work around https://github.com/scullyio/scully/issues/92

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { AppLayoutComponent } from './app-layout.component';
 
 export const appRoutes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'blog' },
   {
     path: '',
     component: AppLayoutComponent,
     children: [
-      { path: '', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
+      { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
       { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
     ],
   },

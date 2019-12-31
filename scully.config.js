00a11444dab8c2f345ef932d99779ad75dbefa74
Bram Borggreve <borggreve@gmail.com>
2019-12-31 17:31:29 -0500

    Remove existing blog

--- scully.config.js
+++ scully.config.js
@@ -1,11 +1,4 @@
 exports.config = {
   projectRoot: './src/app',
-  routes: {
-    '/blog/:slug': {
-      type: 'contentFolder',
-      slug: {
-        folder: './blog',
-      },
-    },
-  },
+  routes: {},
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,9 @@ import { Routes } from '@angular/router';
 import { AppLayoutComponent } from './app-layout.component';
 
 export const appRoutes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'blog' },
   {
     path: '',
     component: AppLayoutComponent,
-    children: [
-      { path: 'blog', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
-      { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
-    ],
+    children: [{ path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) }],
   },
 ];
